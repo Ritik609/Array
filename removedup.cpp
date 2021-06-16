946715624b456239747558c3fa67c0400ea4e894
Ritik Kumar <53653417+Ritik609@users.noreply.github.com>
2021-06-16 15:18:13 +0530

    program to remove duplicates

---
+++ removedup.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+string removedup(string s){
+     if(s.length()==0)
+      return "";
+     char ch=s[0];
+     string ans=removedup(s.substr(1));
+     if(ch==ans[0])
+       return ans;
+     return ch+ans;
+}
+int main() {
+	string s="aaaabbbcdeeee";
+    cout<<removedup(s);
+    
+	return 0;
+}
